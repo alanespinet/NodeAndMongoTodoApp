b90368b481fc73cfaa5017afe5dd45ad25d9ac39
aespinet <aelluvetaa@gmail.com>
2017-08-18 15:28:30 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+
+var app = express();
+
+app.listen(3000, () => { console.log('Server running at port 3000'); });
