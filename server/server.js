f53f43d580d758c6d2502801d9717760c4e3854d
aespinet <aelluvetaa@gmail.com>
2017-08-18 19:53:50 -0400

    Added PACTH route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // Third party modules
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
@@ -93,6 +94,36 @@ app.delete('/todos/:id', (req, res) => {
   })
   .catch( (e) => {
     res.status(400).send();
+  });
+
+});
+
+
+// PATCH REQUEST
+app.patch('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if( !ObjectID.isValid(id) ){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed && body.completed)){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch( (e) => {
+    res.send(e);
   });
 
 });
