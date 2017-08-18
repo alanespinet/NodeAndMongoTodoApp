4933d97ce97eefe3ecebcb306c49326c8f5025d8
aespinet <aelluvetaa@gmail.com>
2017-08-18 17:23:47 -0400

    CRUD Create and Read added. Update and Delete still missing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,77 @@
+// Third party modules
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
+
+// Own modules
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 
+// Initialization process
 var app = express();
 
+
+// Moddlewares
+app.use(bodyParser.json());
+
+
+
+// Requests
+
+// POST
+app.post('/todos', (req, res) => {
+
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then( (doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+
+// GET ALL REQUEST
+app.get('/todos', (req, res) => {
+
+  Todo.find().then( (todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+
+// GET ONE REQUEST
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if( !ObjectID.isValid(id) ){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then( (todo) => {
+
+    if( !todo ){
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  })
+  .catch( (e) => {
+    res.status(400).send();
+  });
+
+});
+
+
+// Server listen
 app.listen(3000, () => { console.log('Server running at port 3000'); });
